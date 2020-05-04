d5d6bb65462a0c979d3fa8fceabb8e273a6b07b0
Ndonye Mark <ndonyemark@gmail.com>
2020-05-05 00:23:53 +0300

    set all configuration files

--- app/request.py
+++ app/request.py
@@ -4,3 +4,9 @@ import urllib.request, json
 
 News = news.News
 Articles = news.Articles
+
+api_key = app.config["NEWS_API_URL"]
+# print(api_key)
+base_url = app.config["NEWS_API_BASE_URL"]
+articles_url = app.config["NEWS_API_ARTICLES_URL"]
+# print(base_url)
