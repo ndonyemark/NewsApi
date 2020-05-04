a9b760280f1049f45e907a2f292a997e2121d3ee
Ndonye Mark <ndonyemark@gmail.com>
2020-05-05 00:20:55 +0300

    add external configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config=True)
 
 app.config.from_object(DevConfig)
-
+app.config.from_pyfile('config.py')
 
 bootstrap = Bootstrap(app)
 
