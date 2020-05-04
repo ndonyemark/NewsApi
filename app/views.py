98f9d1713625c4fdc06d2db4a63bbb2bf4702ded
Ndonye Mark <ndonyemark@gmail.com>
2020-05-05 00:28:56 +0300

    add the index view function

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from app import app
 from flask import render_template
 from .request import get_sources, get_articles
+
+@app.route("/")
+def index():
+
+    general = get_sources('general')
+    business_source_data = get_sources('business')
+    sports_source_data = get_sources('sports')
+    entertainment_source_data = get_sources('entertainment')
+    title = "The Times"
+    return render_template("index.html", title = title, business_news_sources = business_source_data, sports_news_sources = sports_source_data, general_news_sources = general, entertainment_news_sources = entertainment_source_data)
